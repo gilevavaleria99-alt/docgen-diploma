92e3e4abef3ba652718f69be550cc69417dcc9a7
Valeria G <gilevavaleria99@gmail.com>
2026-01-31 19:28:13 +0700

    Функционал: Добавлена возможность установки приложения (PWA)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa' // 1. Импортируем плагин
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 2. Добавляем плагин с настройками
+    VitePWA({
+      registerType: 'autoUpdate', // Автоматически обновлять приложение
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'LABGEN',
+        short_name: 'LABGEN',
+        description: 'Генератор отчетов за одну сессию',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
